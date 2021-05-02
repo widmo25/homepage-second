ef7622a22264f950c12c44fbc5c5443a5b962fd1
Michał Wodniczak <michal.wodniczak@o2.pl>
2021-05-02 13:22:42 +0200

    refactory script

--- script.js
+++ script.js
@@ -2,24 +2,16 @@
     const welcome = () => {
         console.log("Hello World!");
     }
-    
 
     const toggleBackground = () => {
         const body = document.querySelector(".js-theme");
-        const onChangeTheme = document.querySelector(".js-buttonTheme")
-        onChangeTheme.addEventListener("click", () => {
-            body.classList.toggle("darkTheme");
-            onChangeTheme.innerText === "Włącz Ciemny Motyw" ? (onChangeTheme.innerText = "Wyłącz Ciemny Motyw") : (onChangeTheme.innerText = "Włącz Ciemny Motyw");
-        });
+        body.classList.toggle("darkTheme");
+        onChangeTheme.innerText === "Włącz Ciemny Motyw" ? (onChangeTheme.innerText = "Wyłącz Ciemny Motyw") : (onChangeTheme.innerText = "Włącz Ciemny Motyw");
     }
 
-    toggleBackground();
-
     const showHiddenElement = () => {
-        const onSecretButton = document.querySelector(".js-secret");
         const hiddenSpan = document.querySelector(".js-hiddenSpan");
-        onSecretButton.addEventListener("click", () => {
-            hiddenSpan.innerHTML = (`<img width="300" class="image"
+        hiddenSpan.innerHTML = (`<img width="300" class="image"
         src="/image/samotnia.jpg"
         alt="Schronisko Samotnia"><span>
         <p>Adres schroniska</p>
@@ -28,22 +20,21 @@
         58-550 Karpacz
         </span>
         `);
-        });
-    }
-    showHiddenElement();
+    };
 
     const destroyBody = () => {
-        const onDestroyAll = document.querySelector(".js-destroyButton");
         const container = document.querySelector(".js-container");
-        onDestroyAll.addEventListener("click", () => {
-            container.remove();
-        })
-    }
-    destroyBody();
+        container.remove();
+    };
 
-    const init = () =>{
+    const init = () => {
         welcome();
-        
+        const onChangeTheme = document.querySelector(".js-buttonTheme");
+        onChangeTheme.addEventListener("click", toggleBackground);
+        const onSecretButton = document.querySelector(".js-secret");
+        onSecretButton.addEventListener("click", showHiddenElement);
+        const onDestroyAll = document.querySelector(".js-destroyButton");
+        onDestroyAll.addEventListener("click", destroyBody);
     }
     init();
 };
