4f168a223a6df6c1f9808e6f632146f4ab79a7b0
Michał Wodniczak <michal.wodniczak@o2.pl>
2021-05-02 12:21:19 +0200

    fixed image

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-let body = document.querySelector(".js-theme");
-let container = document.querySelector(".js-container");
-let onChangeTheme = document.querySelector(".js-buttonTheme")
-let onSecretButton = document.querySelector(".js-secret");
-let hiddenSpan = document.querySelector(".js-hiddenSpan");
-let onDestroyAll = document.querySelector(".js-destroyButton");
+{
+    const welcome = () => {
+        console.log("Hello World!");
+    }
+    
 
-onChangeTheme.addEventListener("click", () => {
-    body.classList.toggle("darkTheme");
-    onChangeTheme.innerText === "Włącz Ciemny Motyw" ? (onChangeTheme.innerText = "Wyłącz Ciemny Motyw") : (onChangeTheme.innerText = "Włącz Ciemny Motyw");
-});
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-theme");
+        const onChangeTheme = document.querySelector(".js-buttonTheme")
+        onChangeTheme.addEventListener("click", () => {
+            body.classList.toggle("darkTheme");
+            onChangeTheme.innerText === "Włącz Ciemny Motyw" ? (onChangeTheme.innerText = "Wyłącz Ciemny Motyw") : (onChangeTheme.innerText = "Włącz Ciemny Motyw");
+        });
+    }
 
-onSecretButton.addEventListener("click", () => {
-    hiddenSpan.innerHTML = (`<img width="300" class="image"
-    src="https://scontent.fpoz1-1.fna.fbcdn.net/v/t1.6435-9/69071853_2846897295325084_1888346285094731776_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=e3f864&_nc_ohc=0Nd508s0jn4AX9-502W&_nc_ht=scontent.fpoz1-1.fna&oh=6d7d01ab0067a1013bcafc82b71313b9&oe=608CE2C8"
-    alt="Schronisko Samotnia"><span>
-    <p>Adres schroniska</p>
-    Na śniężkę 16
-    <br>
-    58-550 Karpacz
-    </span>
-    `);
-});
+    toggleBackground();
 
-onDestroyAll.addEventListener("click", () => {
-    container.remove();
-})
+    const showHiddenElement = () => {
+        const onSecretButton = document.querySelector(".js-secret");
+        const hiddenSpan = document.querySelector(".js-hiddenSpan");
+        onSecretButton.addEventListener("click", () => {
+            hiddenSpan.innerHTML = (`<img width="300" class="image"
+        src="/image/samotnia.jpg"
+        alt="Schronisko Samotnia"><span>
+        <p>Adres schroniska</p>
+        Na śniężkę 16
+        <br>
+        58-550 Karpacz
+        </span>
+        `);
+        });
+    }
+    showHiddenElement();
 
+    const destroyBody = () => {
+        const onDestroyAll = document.querySelector(".js-destroyButton");
+        const container = document.querySelector(".js-container");
+        onDestroyAll.addEventListener("click", () => {
+            container.remove();
+        })
+    }
+    destroyBody();
+
+    const init = () =>{
+        welcome();
+        
+    }
+    init();
+};
